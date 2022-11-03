ad5255cd053f48f585dd05008aaaaad69e67cba9
Tsvetislava Yankova <tsvetislava.yankova@gmail.com>
2022-11-03 14:59:36 +0200

    Addition of DB Apps Introduction - Exercise

---
+++ Spring Data/02DB Apps Introduction - Exercise/DBAppsExercises/src/Constants.java
@@ -0,0 +1,16 @@
+
+enum Constants {
+    ;
+
+    static final String USER_KEY = "user";
+    static final String USER_VALUE = "root";
+
+    static final String PASSWORD_KEY = "password";
+    static final String PASSWORD_VALUE = "root";
+
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/minions_db";
+
+    static final String COLUMN_LABEL_NAME = "name";
+    static final String COLUMN_LABEL_AGE = "age";
+
+}
